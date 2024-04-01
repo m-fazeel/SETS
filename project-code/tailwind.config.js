a3c79344b9c330b05493ac8032b04e83d17553e1
m-fazeel <102531127+m-fazeel@users.noreply.github.com>
2024-04-01 15:13:43 -0500

    added colors

--- project-code/tailwind.config.js
+++ project-code/tailwind.config.js
@@ -7,12 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+      colors: {
+        'status-yellow': '#FBBF24', // Example color for "In Use"
+        'status-red': '#EF4444',    // Example color for "Maintenance"
+        'status-green': '#22C55E',  // Example color for "Available"
+      }
+    }
   },
   plugins: [],
-};
+}
