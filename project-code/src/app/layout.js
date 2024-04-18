c66911f52b76a5b92ebf2e54b19b893b36c3265c
m-fazeel <102531127+m-fazeel@users.noreply.github.com>
2024-04-18 11:22:42 -0500

    changed the side nav bar and added dark mode

--- project-code/src/app/layout.js
+++ project-code/src/app/layout.js
@@ -2,6 +2,14 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import SideNav from '@/components/side-nav';
 import MaxWidthWrapper from '@/components/max-width-wrapper';
+import { Inter as FontSans } from "next/font/google"
+import { cn } from "@/lib/utils"
+import { ThemeProvider } from "@/components/theme-provider"
+ 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,14 +21,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <body className={inter.className}>
-      {/* <MaxWidthWrapper> */}
+    <body className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}>
+        {/* <MaxWidthWrapper> */}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
         <div className="flex">
           <SideNav />
           <main className="flex-1">{children}</main>
         </div>
       {/* </MaxWidthWrapper> */}
-      {/* <BottomNav /> */}
+          {/* <BottomNav /> */}
+          </ThemeProvider>
     </body>
   </html>
   );
