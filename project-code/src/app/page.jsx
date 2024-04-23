24c94dfa568ed1e50d22cd224d0963e53cc83c57
m-fazeel <102531127+m-fazeel@users.noreply.github.com>
2024-04-23 00:43:23 -0500

    adjusted about us formatting

--- project-code/src/app/page.jsx
+++ project-code/src/app/page.jsx
@@ -9,10 +9,15 @@ import { Grid, Card, Text } from '@nextui-org/react';
 const Home = () => {
 
   return (
-    <div
-      className="flex flex-col pt-4 sm:ml-[120px] md:ml-[250px] sm:border-r sm:border-zinc-700 pb-20 h-full">
+    <div className="flex flex-col">
+
+      <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+      </header>
+      <div>About Us</div>
 
     </div>
+
+
   
   );
 }
