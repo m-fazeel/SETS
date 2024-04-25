c865c06b2747b16d0b17f6b379f250d6ea3c6fdc
m-fazeel <102531127+m-fazeel@users.noreply.github.com>
2024-04-25 02:59:26 -0500

    misc changes

--- project-code/src/app/page.jsx
+++ project-code/src/app/page.jsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/react";
 import { Grid, Card, Text } from '@nextui-org/react';
-
+import img from '../../assets/system.drawio.png'
 
 
 const Home = () => {
