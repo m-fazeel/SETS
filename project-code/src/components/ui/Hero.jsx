1c335699c60a9578791cdfe8aeb917a107e8a185
fxs9905 <fxs9905@mavs.uta.edu>
2024-04-25 00:35:02 -0500

    final about us page

--- project-code/src/components/ui/Hero.jsx
+++ project-code/src/components/ui/Hero.jsx
@@ -4,27 +4,20 @@ import setsicon from '../../../public/setsicon.png'
 import '../sonum.css'
 
 
-const Hero = ({ title, imageUrl }) => {
+const Hero = ({imageUrl }) => {
   return (
     <>
       <div className="hero container">
         <div className="banner">
-          <h1>{title}</h1>
           <p>
-          The Surgical Equipment Tracking System offers a revolutionary software solution, 
-          meticulously designed to address the specific challenges faced by Texas
+           We offer a revolutionary software solution, 
+           designed to address procedural delays and equipment 
+           mismanagement faced by Texas
            Health Arlington Memorial Hospital in surgical equipment management. 
-           This system targets the root causes of procedural delays and equipment 
-           mismanagement, aiming to substantially enhance patient safety through a 
-           state-of-the-art real-time tracking mechanism. 
-           Utilizing cutting-edge Radio-Frequency Identification (RFID) technology, 
-           the system ensures precise monitoring of surgical instruments and devices. 
-           Each item is equipped with a passive RFID tag, seamlessly integrated 
-           into a robust network of RFID readers positioned across surgical departments.
-           The captured data is processed by advanced middleware, culminating in a 
-           user-friendly, web-based application for hospital staff. This innovative approach not 
-           only streamlines equipment utilization but also
-           sets a new standard for operational efficiency and inventory control in healthcare settings.
+           We aim to enhance patient safety through a 
+           real-time tracking mechanism. 
+           Using Radio-Frequency Identification (RFID) technology, 
+           the system ensures precise monitoring of the order trays. 
 
 
           </p>
